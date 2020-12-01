9c1e2e989cbc818dde69640b336f6d093ce0d1fd
Daniel Iovescu <iovescu.dani@gmail.com>
2020-12-01 20:34:13 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Color color = Colors.green;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Transition')),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () {
+            print('test');
+            setState(() {
+              color = color == Colors.green ? Colors.red : Colors.green;
+            });
+          },
+        ),
+        body: Container(
+          child: AnimatedContainer(
+              duration: Duration(milliseconds: 250),
+              width: 250.0,
+              height: 250.0,
+              color: color),
+        ),
+      ),
+    );
+  }
+}
